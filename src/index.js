a8df43eaad60e8b54851bcc7ff2379094c86aca7
Alex_Kelly <afkelly14@Gmail.com>
2020-10-18 21:29:17 -0400

    setting up files and pulling the stock market api

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import api from './api';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const IndexPage = () => {
+    // Create state variables
+    let [responseData, setResponseData] = React.useState('')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // fetches data
+    const fetchData = (e) => {
+        e.preventDefault()
+
+        api.getData()
+        .then((response)=>{
+            setResponseData(response.data)
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+
+    return (
+        <div>
+            <h1>{responseData.title}</h1>
+
+            <button onClick={(e) => fetchData(e)} type='button'>Click Me For Data</button>
+
+            {responseData.dates && responseData.dates.map(date => {
+                return <p>{date}</p>
+            })}
+        </div>
+    )
+}
+
+export default IndexPage
