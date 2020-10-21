fb1ac9064085f8ffa7d85054a28e4e511bd2254a
Alex_Kelly <afkelly14@Gmail.com>
2020-10-21 16:45:32 -0400

    small changes to api call

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-          
+          data: []
         };
       }
 render() {
@@ -17,11 +17,11 @@ render() {
 axios ({
   method: 'GET',
   url: 'https://rapidapi.p.rapidapi.com/time_series',
-  params: [{symbol: 'AMZN', interval: '1day', outputsize: '30', format: 'json'}],
-  headers: [{
+  params: {symbol: 'AMZN', interval: '1day', outputsize: '30', format: 'json'},
+  headers: {
     'x-rapidapi-host': 'twelve-data1.p.rapidapi.com',
     'x-rapidapi-key': '2b8d3472bamshb3c4f5ef0be4828p1ba36ejsnb64c20deed5d'
-  }]
+  }
 }
 
 // axios.request(options).then(function (response) {
